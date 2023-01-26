62fef2d5cd77d8f2a3f775ac93d7e11f446db909
rcshpark <rcshpark@gmail.com>
2023-01-26 16:14:44 +0900

    게시판 데이터 저장

---
+++ lib/page/addPostScreen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shcoolapp/provider/board_provider.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  var _titleTextEditController = TextEditingController();
+  var _contentTextEditController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<BoardProvider>(context);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('글쓰기'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "제목",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _titleTextEditController,
+                decoration: InputDecoration(
+                    fillColor: Colors.grey,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                onChanged: (inputLoc) {
+                  // location = inputLoc;
+                },
+                maxLines: 1,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "내용",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _contentTextEditController,
+                decoration: InputDecoration(
+                    fillColor: Colors.grey,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                onChanged: (inputCon) {
+                  // content = inputCon;
+                },
+                maxLines: 5,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    provider.insertData(_titleTextEditController.text,
+                        _contentTextEditController.text);
+                  },
+                  child: const Text("저장"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/provider/board_provider.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+import 'package:shcoolapp/model/board_model.dart';
+
+class BoardProvider with ChangeNotifier {
+  final firestore = FirebaseFirestore.instance;
+
+  List<BoardModel> _boardModel = [];
+
+  List<BoardModel> get boardModel => _boardModel;
+// 데이터 불러오기
+  dynamic fetchData() async {
+    var result =
+        await firestore.collection('board').doc('f5MElAaNOaCEvZ3mKMNl').get();
+    print(result);
+  }
+
+  void insertData(String title, String content) async {
+    try {
+      dynamic data = {'title': title, 'location': content};
+      firestore.collection('board').add(data);
+    } catch (e) {
+      return null;
+    }
+  }
+}
+// 데이터 저장하기
+
+  // void insertData(String location, String content) async {
+  //   try {
+  //     dynamic data = {'location': location, 'content': content};
+  //     String jsonString = jsonEncode(data);
+  //     var response = await http.post(Uri.parse(baseUrl),
+  //         headers: {"Content-Type": "application/json"}, body: jsonString);
+  //     notifyListeners();
+  //     print("body:${response.body}");
+  //   } catch (e) {
+  //     return null;
+  //   }
+  // }
+
