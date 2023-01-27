f222f424a4874f9762941970eb88527e16f193ce
rcshpark <rcshpark@gmail.com>
2023-01-27 22:59:39 +0900

    feat: firebase data -> 리스트 화면

--- lib/page/boardScreen.dart
+++ lib/page/boardScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -22,17 +23,69 @@ class _BoardScreenState extends State<BoardScreen> {
         title: const Text('게시판'),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => AddPostScreen(id: widget.id)));
-          },
-          child: Text(widget.id),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AddPostScreen(id: widget.id)));
+                },
+                child: Text(widget.id),
+              ),
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection('board').snapshots(),
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => listTitle(
+                        snapshot.data!.docs[index]['title'],
+                        snapshot.data!.docs[index]['content']),
+                    itemCount: snapshot.data!.docs.length,
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                  );
+                }))
+          ],
         ),
       ),
     );
   }
+}
+
+Widget listTitle(String title, String content) {
+  return Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: GestureDetector(
+      onTap: () {
+        print('#########');
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '제목 : $title',
+            style: const TextStyle(fontSize: 15.0, color: Colors.black),
+          ),
+          Text(
+            '내용 : $content',
+            overflow: TextOverflow.visible,
+            style: const TextStyle(fontSize: 15.0, color: Colors.black),
+          ),
+        ],
+      ),
+    ),
+  );
 }
