ddb8b502c13b552141f40cc93340e6d5c196875e
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-01 23:51:39 +0900

    캘린더 수정
    이벤트 로더 하나남음

--- lib/page/calendarpage.dart
+++ lib/page/calendarpage.dart
@@ -15,6 +15,7 @@ class _CalendarPageState extends State<CalendarPage> {
   var pk = const Uuid().v1();
 
   late Map<DateTime, List<Event>> selectedEvents;
+
   DateTime today = DateTime.now();
 
   void _onDaySelected(DateTime day, DateTime focusDay) {
@@ -25,21 +26,11 @@ class _CalendarPageState extends State<CalendarPage> {
 
   final TextEditingController _eventController = TextEditingController();
 
-  // @override
-  // void initState() {
-  //   selectedEvents = {};
-  //   super.initState();
-  // }
-
   @override
   void dispose() {
     _eventController.dispose();
     super.dispose();
   }
-
-  // List<Event> _getEventsfromDay(DateTime date) {
-  //   return selectedEvents[date] ?? [];
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -64,32 +55,13 @@ class _CalendarPageState extends State<CalendarPage> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              // TextButton(
-                              //     onPressed: () => Navigator.pop(context),
-                              //     child: const Text('취소')),
                               TextButton(
-                                child: const Text('확인'),
-                                onPressed: () =>
-                                    _addEvent(Event(_eventController.text)),
-                                // onPressed: () {
-                                //   if (_eventController.text.isEmpty) {
-                                //   } else {
-                                //     if (selectedEvents[_onDaySelected] !=
-                                //         null) {
-                                //       selectedEvents[today]!.add(
-                                //           Event(title: _eventController.text));
-                                //     } else {
-                                //       selectedEvents[today] = [
-                                //         Event(title: _eventController.text)
-                                //       ];
-                                //     }
-                                //   }
-                                //   Navigator.pop(context);
-                                //   _eventController.clear();
-                                //   setState(() {});
-                                //   return;
-                                // },
-                              )
+                                  onPressed: () => Navigator.pop(context),
+                                  child: const Text('취소')),
+                              TextButton(
+                                  child: const Text('확인'),
+                                  onPressed: () =>
+                                      _addEvent(Event(_eventController.text)))
                             ],
                           )
                         ],
@@ -124,13 +96,12 @@ class _CalendarPageState extends State<CalendarPage> {
                         lastDay: DateTime.utc(2030, 10, 16),
                         onDaySelected: _onDaySelected,
                       ),
-                      SizedBox(
-                        height: 300,
+                      Expanded(
                         child: ListView(
                           children:
                               documents.map((doc) => _buildItem(doc)).toList(),
                         ),
-                      ),
+                      )
                     ],
                   ),
                 );
@@ -170,130 +141,4 @@ class _CalendarPageState extends State<CalendarPage> {
       _eventController.text = "";
     });
   }
-
-  // ignore: non_constant_identifier_names
-  // Widget CalendarWidget() {
-  //   StreamBuilder<QuerySnapshot>(
-  //       stream: FirebaseFirestore.instance.collection('$pk.$today').snapshots(),
-  //       builder: (context, snapshot) {
-  //         if (!snapshot.hasData) {
-  //           return const CircularProgressIndicator();
-  //         }
-  //         final documents = snapshot.data!.docs;
-
-  //         return Padding(
-  //           padding: const EdgeInsets.all(8.0),
-  //           child: Column(
-  //             children: [
-  //               Text(
-  //                 '선택한 날짜: ${today.toString().split(" ")[0]}',
-  //                 style: const TextStyle(
-  //                     fontFamily: 'salt', color: Colors.black, fontSize: 30),
-  //               ),
-  //               Column(children: [
-  //                 TableCalendar(
-  //                   locale: 'ko_KR',
-  //                   daysOfWeekHeight: 20,
-  //                   rowHeight: 46,
-  //                   headerStyle: const HeaderStyle(
-  //                       formatButtonVisible: false, titleCentered: true),
-  //                   availableGestures: AvailableGestures.all,
-  //                   selectedDayPredicate: ((day) => isSameDay(day, today)),
-  //                   focusedDay: today,
-  //                   firstDay: DateTime.utc(2010, 10, 16),
-  //                   lastDay: DateTime.utc(2030, 10, 16),
-  //                   onDaySelected: _onDaySelected,
-  //                 ),
-  //                 Expanded(
-  //                   child: ListView(
-  //                     children:
-  //                         documents.map((doc) => _buildItem(doc)).toList(),
-  //                   ),
-  //                 )
-  //               ]),
-  //             ],
-  //           ),
-  //         );
-  //       });
-  // return Padding(
-  //   padding: const EdgeInsets.all(10.0),
-  //   child: Column(
-  //     children: [
-  //       Text(
-  //         '선택한 날짜: ${today.toString().split(" ")[0]}',
-  //         style: const TextStyle(
-  //             fontFamily: 'salt', color: Colors.black, fontSize: 30),
-  //       ),
-  //       Column(
-  //         children: [
-  //           TableCalendar(
-  //             locale: 'ko_KR',
-  //             daysOfWeekHeight: 20,
-  //             rowHeight: 46,
-  //             headerStyle: const HeaderStyle(
-  //                 formatButtonVisible: false, titleCentered: true),
-  //             availableGestures: AvailableGestures.all,
-  //             selectedDayPredicate: ((day) => isSameDay(day, today)),
-  //             focusedDay: today,
-  //             firstDay: DateTime.utc(2010, 10, 16),
-  //             lastDay: DateTime.utc(2030, 10, 16),
-  //             onDaySelected: _onDaySelected,
-  //             // eventLoader: _getEventsfromDay,
-  //           ),
-  //           // StreamBuilder<QuerySnapshot>(
-  //           //     stream: FirebaseFirestore.instance
-  //           //         .collection('$pk.$today')
-  //           //         .snapshots(),
-  //           //     builder: (context, snapshot) {
-  //           //       if (!snapshot.hasData) {
-  //           //         return const CircularProgressIndicator();
-  //           //       }
-  //           //       final documents = snapshot.data!.docs;
-  //           //       return Expanded(
-  //           //         child: ListView(
-  //           //           children:
-  //           //               documents.map((doc) => _buildItem(doc)).toList(),
-  //           //         ),
-  //           //       );
-  //           //     }),
-  //           // ..._getEventsfromDay(today).map((Event event) => ListTile(
-  //           //       title: Text(event.title),
-  //           //     ))
-  //         ],
-  //       ),
-  //     ],
-  //   ),
-  // );
 }
-
-  // Widget _buildItem(DocumentSnapshot snapshot) {
-  //   final event = Event(
-  //     snapshot['title'],
-  //   );
-  //   return ListTile(
-  //     title: Text(
-  //       event.title,
-  //     ),
-  //     trailing: IconButton(
-  //       icon: const Icon(Icons.delete_forever),
-  //       onPressed: () => _deleteEvent(snapshot),
-  //     ),
-  //   );
-  // }
-
-  // void _deleteEvent(DocumentSnapshot snapshot) {
-  //   FirebaseFirestore.instance
-  //       .collection('$pk.$today')
-  //       .doc(snapshot.id)
-  //       .delete();
-  // }
-
-  // void _addEvent(Event event) {
-  //   setState(() {
-  //     FirebaseFirestore.instance
-  //         .collection('$pk.$today')
-  //         .add({'title': event.title});
-  //     _eventController.text = "";
-  //   });
-  // }
-// }
