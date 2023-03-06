3209599107bf498785fbdd0fd1e3b6cc42ff7053
rcshpark <rcshpark@gmail.com>
2023-03-06 22:02:27 +0900

    버튼 이미지 삽입

--- lib/page/kakao_loginScreen.dart
+++ lib/page/kakao_loginScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shcoolapp/controller/kakao_controller.dart';
 import 'package:shcoolapp/page/chatting_room.dart';
+import 'package:shcoolapp/page/mainpage.dart';
 
 class KakaoLoginScreen extends StatefulWidget {
   const KakaoLoginScreen({super.key});
@@ -14,17 +15,22 @@ class _KakaoLoginScreenState extends State<KakaoLoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
           onPressed: () async {
             var provider = LoginProvider();
             String? name = await provider.kakaoLogin();
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => ChattingRoom(room: '', name: name)));
+            // 카카오 로그인때 이름 안넘어오면 에러문구 설정 해야함
+            if (mounted) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MainPage(name: name)));
+            }
           },
-          child: Text('kakao login'),
+          child: Image.asset('assets/kakao.png'),
         ),
       ),
     );
