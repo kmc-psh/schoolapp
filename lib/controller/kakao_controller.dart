6a69faea7017a5a57cc13c5af7ec54611df86e6d
rcshpark <rcshpark@gmail.com>
2023-03-09 13:50:58 +0900

    fix 로그인 로직 수정...

--- lib/controller/kakao_controller.dart
+++ lib/controller/kakao_controller.dart
@@ -14,7 +14,7 @@ class LoginProvider with ChangeNotifier {
   dynamic _email;
   dynamic get test => _email;
 
-  UserModel _userModel = UserModel();
+  // UserModel _userModel = UserModel();
 
   dynamic _pk;
   int get pk => _pk;
@@ -71,45 +71,82 @@ class LoginProvider with ChangeNotifier {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     User user = await UserApi.instance.me();
     var kakaoProfile = user.kakaoAccount!.profile!.nickname;
+    var profileImageUrl = user.kakaoAccount!.profile!.profileImageUrl;
     _email = user.kakaoAccount!.email;
     _pk = user.id;
-    var validUser = firestore.collection('회원정보').doc(kakaoProfile).id;
     var name;
-    await firestore.collection('회원정보').doc(kakaoProfile).get().then((value) {
-      if (value.data() == null) {
-        FirebaseFirestore.instance
-            .collection('회원정보')
-            .doc(_email)
-            .set({'pk': user.id, '카카오 프로필': kakaoProfile, '카카오 계정': _email});
+    var userData = await firestore.collection('회원정보').doc(_email).get();
 
-        name = kakaoProfile;
-        return name;
-      }
-      dynamic user_pk = value.data();
-      _userModel = UserModel.fromJson(user_pk);
-      // doc 에 동명인이 있을경우
-      if (_email == validUser) {
-        // 동명인이 자기일 경우
-        if (user.id == _userModel.pk) {
-          name = validUser;
+    print(userData.data());
 
+    if (userData.data() == null) {
+      FirebaseFirestore.instance.collection('회원정보').doc(_email).set({
+        'pk': user.id,
+        '카카오 프로필': kakaoProfile,
+        '카카오 계정': _email,
+        '프로필 이미지': profileImageUrl
+      });
+      name = kakaoProfile;
+      return name;
+    } else {
+      if (_email == userData.data()!['카카오 계정']) {
+        if (_pk == userData['pk']) {
+          name = kakaoProfile;
           return name;
         } else {
           FirebaseFirestore.instance.collection('회원정보').doc(_email).set({
             'pk': user.id,
             '카카오 프로필': kakaoProfile,
             '카카오 계정': _email,
+            '프로필 이미지': profileImageUrl
           });
-          name = FirebaseFirestore.instance
-              .collection('회원정보')
-              .doc(kakaoProfile)
-              .id;
+          name = kakaoProfile;
           return name;
         }
       }
-      return null;
-    });
+    }
 
-    return name;
+    //fix
+    //   await firestore.collection('회원정보').doc(kakaoProfile).get().then((value) {
+    //     if (value.data() == null) {
+    //       FirebaseFirestore.instance.collection('회원정보').doc(_email).set({
+    //         'pk': user.id,
+    //         '카카오 프로필': kakaoProfile,
+    //         '카카오 계정': _email,
+    //         '프로필 이미지': profileImageUrl
+    //       });
+
+    //       name = kakaoProfile;
+    //       notifyListeners();
+    //       return name;
+    //     }
+    //     dynamic user_pk = value.data();
+    //     // _userModel = UserModel.fromJson(user_pk);
+    //     // doc 에 동명인이 있을경우
+    //     if (_email == validUser) {
+    //       // 동명인이 자기일 경우
+    //       if (user.id == user_pk['pk']) {
+    //         name = validUser;
+
+    //         return name;
+    //       } else {
+    //         FirebaseFirestore.instance.collection('회원정보').doc(_email).set({
+    //           'pk': user.id,
+    //           '카카오 프로필': kakaoProfile,
+    //           '카카오 계정': _email,
+    //           '프로필 이미지': profileImageUrl
+    //         });
+    //         name = FirebaseFirestore.instance
+    //             .collection('회원정보')
+    //             .doc(kakaoProfile)
+    //             .id;
+    //         notifyListeners();
+    //         return name;
+    //       }
+    //     }
+    //     return null;
+    //   });
+
+    //   return name;
   }
 }
